566a666131499f922d870b7b08bed50831862193
Stephen Spalding <sspalding@netflix.com>
2020-06-29 18:04:49 -0700

    Version 2.0.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljstache "2.0.6"
+(defproject cljstache "2.0.7-SNAPSHOT"
   :min-lein-version "2.5.2"
   :description "{{ mustache }} for Clojure[Script]"
   :url "http://github.com/fotoetienne/cljstache"
